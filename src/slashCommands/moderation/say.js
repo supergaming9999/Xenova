19d8d171c5585104e1f66428e2755dabb6f48fc9
BuildTools <unconfigured@null.spigotmc.org>
2023-10-23 20:18:00 -0400

    Remove role slashCommand and add say slashCommand

---
+++ src/slashCommands/moderation/say.js
@@ -0,0 +1,33 @@
+const { ApplicationCommandType, EmbedBuilder, ApplicationCommandOptionType, ChatInputCommandInteraction } = require('discord.js');
+const DiscordClient = require('../../structures/DiscordClient');
+const Util = require('../../util/Util');
+
+module.exports = {
+	name: 'say',
+	description: "Make me say something.",
+	type: ApplicationCommandType.ChatInput,
+    default_member_permissions: "ManageMessages",
+	cooldown: 3000,
+    options: [
+        {
+            name: "text",
+            description: "What do you want me to say?",
+            type: ApplicationCommandOptionType.String,
+            required: true
+        }
+    ],
+    /**
+     * @param {DiscordClient} client 
+     * @param {ChatInputCommandInteraction} interaction 
+     * @param {Util.configModel} config 
+     * @param {Util.dbModel} profileData
+     */
+	run: async (client, interaction, config, profileData) => {
+        var text = interaction.options.getString("text");
+        await interaction.channel.send({ content: text }).catch(console.error);
+        var embed = new EmbedBuilder()
+            .setColor('Green')
+            .setDescription(`Successfully said \`${text}\``);
+        return interaction.reply({ embeds: [embed], ephemeral: true });
+	}
+};
